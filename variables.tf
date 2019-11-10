0027a1015ce8392756efa4296a901543b9a28a96
lilibkurian <lilib.kurian@gmail.com>
2019-11-10 18:09:34 -0500

    deleting directories

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable modname {}
+variable SubnetId {
+default = "subnet-fa632fd4"
+ description = "The subnet id of a default vpc in the working region"
+}
+variable SecGroupId {
+description = "this variable doesn't have a default value but it is passed from the main code"
+}
