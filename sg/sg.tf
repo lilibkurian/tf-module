7df97ac96399cd238fc4a8c6068a810db5f688ef
lilib.kurian <lilib.kurian@gmail.com>
2020-04-23 11:16:13 -0400

    sg

---
+++ sg/sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "example" {
+  name        = var.sg_name
+  description = var.sg_desc
+  vpc_id      = var.sg_vpcid
+
+  dynamic "ingress" {
+    for_each = toset(var.sg_ingress_ports)
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  dynamic "egress" {
+    for_each = toset(var.sg_egress_ports)
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
+  }
+}
---
+++ sg/variables.tf
@@ -0,0 +1,5 @@
+variable "sg_name" {}
+variable "sg_desc" {}
+variable "sg_vpcid" {}
+variable "sg_ingress_ports" {}
+variable "sg_egress_ports" {}
