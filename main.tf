bd82fde55346934f0918bb14a82422d6ebf364ab
lilib.kurian <lilib.kurian@gmail.com>
2020-05-01 14:21:17 -0400

    desc

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ listener {
 module "sg" {
   source           = "./sg"
   sg_name          = "Test-SG"
-  sg_desc          = "Hi SG"
+  sg_desc          = "Hi SG How are you"
   sg_vpcid         = var.vpcid
   sg_ingress_ports = var.ingress_ports
   sg_egress_ports  = var.egress_ports
