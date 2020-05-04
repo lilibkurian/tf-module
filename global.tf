dc29e381192bab73d73a3f7bd483a9b0999be5a3
lilib.kurian <lilib.kurian@gmail.com>
2020-05-03 21:16:11 -0400

    env19

--- global.tf
+++ global.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   version                 = "~> 2.0"
-  region                  = var.AWS_REGION
+  region                  = var.aws_region
   #shared_credentials_file = var.aws_cred_file
   #profile                 = terraform.workspace
   #access_key = var.AWS_ACCESS_KEY_ID
@@ -8,10 +8,10 @@ provider "aws" {
   
 }
 
-variable AWS_REGION {
-  type        = string
+variable aws_region {
+  #type        = string
   #default     = "us-east-1"
-  description = "To which region should the resources be deployed? ie us-east-1, us-west-2, etc ..."
+  #description = "To which region should the resources be deployed? ie us-east-1, us-west-2, etc ..."
 }
 
 #variable aws_access_key_id {}
