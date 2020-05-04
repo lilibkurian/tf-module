565074842227378fe0aa6b2673d4271cf01ceeda
lilib.kurian <lilib.kurian@gmail.com>
2020-05-03 20:44:31 -0400

    env14

--- global.tf
+++ global.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region                  = var.aws_region
   #shared_credentials_file = var.aws_cred_file
   #profile                 = terraform.workspace
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   
 }
 
@@ -14,13 +14,13 @@ variable aws_region {
   description = "To which region should the resources be deployed? ie us-east-1, us-west-2, etc ...."
 }
 
-variable aws_access_key_id {}
+#variable aws_access_key_id {}
 
-variable aws_secret_access_key {}
+#variable aws_secret_access_key {}
 
-#variable AWS_ACCESS_KEY_ID {}
+variable AWS_ACCESS_KEY_ID {}
 
-#variable AWS_SECRET_ACCESS_KEY {}
+variable AWS_SECRET_ACCESS_KEY {}
 
 
 #variable "aws_cred_file" {}
