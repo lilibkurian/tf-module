e68dd0a417b07b59d763f5a7fd154c74fa4498d3
lilib.kurian <lilib.kurian@gmail.com>
2020-05-05 11:24:42 -0400

    test3

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ listener {
 module "sg" {
   source           = "./sg"
 
-  sg_name          = "Test-SG how is it going1"
+  sg_name          = "Test-SG how is it going2"
   sg_desc          = var.nameabc
 
   sg_vpcid         = var.vpcid
