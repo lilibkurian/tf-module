0c3249ac56bd5fcd4ac7f460989dbd131c4b9829
lilib.kurian <lilib.kurian@gmail.com>
2020-05-06 11:10:09 -0400

    test sg tag

--- sg/sg.tf
+++ sg/sg.tf
@@ -2,6 +2,9 @@ resource "aws_security_group" "example" {
   name        = var.sg_name
   description = var.sg_desc
   vpc_id      = var.sg_vpcid
+  tags        = {
+    Name      = "test"
+  }
 
   dynamic "ingress" {
     for_each = toset(var.sg_ingress_ports)
