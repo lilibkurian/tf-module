17d33228279c7778c543fdb0f992cdc1b491d79e
lilib.kurian <lilib.kurian@gmail.com>
2020-05-05 20:36:58 -0400

    tfe modules

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ listener {
 
 }
 */
+/*
 module "sg" {
   #source           = "./sg"
   source            = "github.com/lilibkurian/terraform-aws-sg"
@@ -25,13 +26,22 @@ module "sg" {
   sg_vpcid         = var.vpcid
   sg_ingress_ports = var.ingress_ports
   sg_egress_ports  = var.egress_ports
-} 
+} */
 
-/*
+
 module "sg" {
   source  = "app.terraform.io/Lilib/sg/aws"
   version = "1.0.0"
+
+  sg_name          = "Test-SG how is it going2"
+  sg_desc          = var.nameabc
+  sg_tags          = local.tags_sg
+
+  sg_vpcid         = var.vpcid
+  sg_ingress_ports = var.ingress_ports
+  sg_egress_ports  = var.egress_ports
+
 }
-*/
+
 
 
