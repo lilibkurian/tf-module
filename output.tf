2e5b74a9dba06960133f89e00adf29a954271670
lilib.kurian <lilib.kurian@gmail.com>
2020-05-21 14:29:02 -0400

    test-14

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "security_group_description" {
+output "security_group_description_1" {
   value = module.sg.security_group_description
+}
+
+output "security_group_description_2" {
+  value = var.nameabc
 }
